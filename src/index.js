1d4ed42dea26db4a3ed79bb18ca6fe94ab4ef0d0
Shu Li <108698293+Cheulie@users.noreply.github.com>
2023-02-22 16:01:40 +0000

    some of the tasks completed

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-require('dotenv').config();
-const fs = require('fs');
-const express = require('express');
+require("dotenv").config();
+const fs = require("fs");
+const express = require("express");
 const app = express();
-const path = require('path');
+const path = require("path");
 const port = 8080;
-const bodyParser = require('body-parser');
-const { v4: uuidv4 } = require('uuid');
+const bodyParser = require("body-parser");
+const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
 
 //Read todos from todos.json into variable
@@ -19,23 +19,30 @@ app.use(bodyParser.json());
 app.use("/content", express.static(path.join(__dirname, "public")));
 
 app.get("/", (_, res) => {
-  /*
-  res.sendFile("./public/index.html", { root: __dirname });
-  */
-  res.status(501).end();
+  res.sendFile("./public/index.html", { root: __dirname }, (err) => {
+    console.log(err);
+});
 });
 
-app.get('/todos', (_, res) => {
-  /*
-  res.header("Content-Type","application/json");
-  res.sendFile(todoFilePath, { root: __dirname });
-  */
+app.get("/todos", (_, res) => {
+  // res.header("Content-Type", "application/json");
+  // res.sendFile(todoFilePath, { root: __dirname }
+  
+
   res.status(501).end();
 });
 
 //Add GET request with path '/todos/overdue'
 
+app.get('/todos/overdue', (req, res) => {
+  res.status(errorStatus).send('Not implemented');
+});
+
 //Add GET request with path '/todos/completed'
+
+app.get('/todos/completed', (req, res) => {
+  res.status(errorStatus).send('Not implemented');
+});
 
 //Add POST request with path '/todos'
 
@@ -43,12 +50,21 @@ app.get('/todos', (_, res) => {
 
 //Add POST request with path '/todos/:id/complete
 
+app.post('/todos/:id/complete', (req, res) => {
+  res.status(errorStatus).send('Not implemented');
+});
+
+
 //Add POST request with path '/todos/:id/undo
 
 //Add DELETE request with path '/todos/:id
 
+app.delete('/todos/:id', (req, res) => {
+  res.status(errorStatus).send('Not implemented');
+});
+
 app.listen(port, function () {
-    console.log(`Node server is running... http://localhost:${port}`);
+  console.log(`Node server is running... http://localhost:${port}`);
 });
 
 module.exports = app;
