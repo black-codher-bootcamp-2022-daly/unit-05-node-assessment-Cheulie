337d2c42135fbda757af6257985fec9f52065521
Shu Li <108698293+Cheulie@users.noreply.github.com>
2023-02-22 21:08:27 +0000

    updated some of the code, task incomplete

--- src/index.js
+++ src/index.js
@@ -3,10 +3,8 @@ const fs = require("fs");
 const express = require("express");
 const app = express();
 const path = require("path");
-<<<<<<< HEAD
+
 const port = 8080;
-=======
->>>>>>> refs/remotes/origin/main
 const bodyParser = require("body-parser");
 const { v4: uuidv4 } = require("uuid");
 const todoFilePath = process.env.BASE_JSON_PATH;
@@ -18,63 +16,80 @@ app.use(bodyParser.json());
 
 app.use("/content", express.static(path.join(__dirname, "public")));
 
+//Add GET request with path '/'
+
 app.get("/", (_, res) => {
   res.sendFile("./public/index.html", { root: __dirname }, (err) => {
     console.log(err);
+  });
+
+  res.status(200);
 });
-});
+
+//Add GET request with path '/todos'
 
 app.get("/todos", (_, res) => {
-<<<<<<< HEAD
-  // res.header("Content-Type", "application/json");
-  // res.sendFile(todoFilePath, { root: __dirname }
-  
-
-=======
-  /*
-  res.header("Content-Type","application/json");
+  res.header("Content-Type", "application/json");
   res.sendFile(todoFilePath, { root: __dirname });
-  */
->>>>>>> refs/remotes/origin/main
-  res.status(501).end();
+
+  res.status(200);
 });
 
 //Add GET request with path '/todos/overdue'
 
-app.get('/todos/overdue', (req, res) => {
-  res.status(errorStatus).send('Not implemented');
+app.get("/todos/overdue", (req, res) => {
+  const date = new Date();
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+  const result = profile.filter((item) => new Date(item.due) < date);
+  res.send(result);
+  profile[0].due;
 });
 
 //Add GET request with path '/todos/completed'
 
-app.get('/todos/completed', (req, res) => {
-  res.status(errorStatus).send('Not implemented');
+app.get("/todos/completed", (req, res) => {
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+  const result = profile.filter((item) => item.completed == true);
+  res.send(result);
 });
 
 //Add POST request with path '/todos'
 
-//Add PATCH request with path '/todos/:id
-
-//Add POST request with path '/todos/:id/complete
-
-app.post('/todos/:id/complete', (req, res) => {
-  res.status(errorStatus).send('Not implemented');
+app.get("/todos/", (req, res) => {
+  const message = "Hello everybody";
+  res.send(message);
 });
 
+//Add PATCH request with path '/todos/:id
+
+app.get("/todos/:id", (req, res) => {
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
+
+  res.send(result);
+});
+//Add POST request with path '/todos/:id/complete
 
 //Add POST request with path '/todos/:id/undo
 
+app.get("/todos/:id/undo", (req, res) => {
+  res.send(message);
+});
 //Add DELETE request with path '/todos/:id
 
-<<<<<<< HEAD
-app.delete('/todos/:id', (req, res) => {
-  res.status(errorStatus).send('Not implemented');
+app.delete("/todos/:01507581-9d12-4c3a-bb60-19d539a11189", (req, res) => {
+  const profile = JSON.parse(
+    fs.readFileSync(path.join(__dirname, "models/todos.json"))
+  );
 });
 
 app.listen(port, function () {
   console.log(`Node server is running... http://localhost:${port}`);
 });
 
-=======
->>>>>>> refs/remotes/origin/main
 module.exports = app;
